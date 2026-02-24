2bfd2d57df75cc54c767f786afa3384c3c6b1746
enjoydarts <minoru@kitayama.dev>
2026-02-24 17:36:25 +0900

    LLM利用ログでembedding用途を許可

---
+++ db/migrations/000014_allow_embedding_purpose_in_llm_usage_logs.up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE llm_usage_logs
+  DROP CONSTRAINT IF EXISTS llm_usage_logs_purpose_check;
+
+ALTER TABLE llm_usage_logs
+  ADD CONSTRAINT llm_usage_logs_purpose_check
+  CHECK (purpose IN ('facts', 'summary', 'digest', 'embedding'));
